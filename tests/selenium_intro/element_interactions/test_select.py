39bea7cd7f8480464e2d864c9106126e8dc8426f
Yanique Dickson <yanique.dickson@gmail.com>
2023-01-06 10:26:36 -0500

    updated select test

--- tests/selenium_intro/element_interactions/test_select.py
+++ tests/selenium_intro/element_interactions/test_select.py
@@ -7,12 +7,21 @@ def test_select(browser):
 
   # Select an element in the first dropdown list based on its value
   menu_1 = browser.find_element(By.ID, 'dropdowm-menu-1')
+
+  # Use select element to initialize Select class
   select_1 = Select(menu_1)
+
+  # Select an element by its value
   select_1.select_by_value('python')
   time.sleep(2)
+  
 
   # Select an element in the first dropdown list based on its index in the list
   menu_2 = browser.find_element(By.ID, 'dropdowm-menu-2')
+
+  # Use select element to initialize Select class
   select_2 = Select(menu_2)
+
+  # Select an element by its index
   select_2.select_by_index(1)
   time.sleep(2)
